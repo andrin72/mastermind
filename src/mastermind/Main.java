8ab1b0b4e7e48ce5c3092bfc3f09f78d5058b625
Andrin Wiedmer <wiedmer.andrin@gmail.com>
2018-09-30 15:54:12 +0200

    init and random method added

--- src/mastermind/Main.java
+++ src/mastermind/Main.java
@@ -1,10 +1,52 @@
 package mastermind;
 
-public class Main {
+import java.util.Random;
 
+public class Main {
+	
+	//Variablendeklarationen
+	private Random random;
+	private String[] colors;
+	private String[] secretCode;
+	
 	public static void main(String[] args) {
 
-		System.out.println("Test Aenderung");
+		Main instance = new Main();
+		instance.init();
+		
+	}
+	
+	private void init() {
+		
+		random = new Random();
+		colors = new String[] {"red", "green", "blue", "yellow", "white", "black" };
+		
+		secretCode = new String[4];
+		
+		for (int i = 0; i < 4; i++) {
+			
+			secretCode[i] = colors[getRandomNumber(0,5)];
+			
+		}
+		
+		printCode();
+		
+	}
+	
+	private int getRandomNumber(int min, int max) {
+		
+	    int randomNumber = random.nextInt((max - min) + 1) + min;
+	    return randomNumber;
+		
+	}
+	
+	private void printCode() {
+		
+		for (int i = 0; i < 4; i++) {
+			
+			System.out.println(secretCode[i]);
+			
+		}	
 		
 	}
 
