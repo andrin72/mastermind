0dc9886f2a1306f0a09296108f2c027943b428d6
Andrin Wiedmer <wiedmer.andrin@gmail.com>
2018-10-02 20:58:15 +0200

    several changes

--- src/mastermind/Main.java
+++ src/mastermind/Main.java
@@ -1,14 +1,19 @@
 package mastermind;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Main {
 	
-	//Variablendeklarationen
+	//declarations of variables
 	private Random random;
-	private String[] colors;
-	private String[] secretCode;
+	private static final char[] COLORS = new char[] {'r', 'g', 'b', 'y', 'w', 's' };
+	private char[] secretCodeGenerated;
+	private char[] secretCodeInput;
+	private InputStreamReader inputStreamReader;
 	
+	//creates instance and calls init() method
 	public static void main(String[] args) {
 
 		Main instance = new Main();
@@ -16,23 +21,53 @@ public class Main {
 		
 	}
 	
+	//initializes most important variables in the beginning of the game
 	private void init() {
 		
+		inputStreamReader = new InputStreamReader(System.in);
 		random = new Random();
-		colors = new String[] {"red", "green", "blue", "yellow", "white", "black" };
 		
-		secretCode = new String[4];
+		secretCodeGenerated = new char[4];
+		secretCodeInput = new char[4];
 		
 		for (int i = 0; i < 4; i++) {
 			
-			secretCode[i] = colors[getRandomNumber(0,5)];
+			secretCodeGenerated[i] = COLORS[getRandomNumber(0,5)];
 			
 		}
 		
-		printCode();
+		printCodeGenerated();
+		
+		System.out.print("Geben Sie den Code ein: ");
+		
+		
+		
+		do {
+			try {
+				readUserInput();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+				
+			}
+		} while (!validateInput());
+		
+		printCodeInput();
+		
+		
 		
 	}
 	
+	//Reads user input in form of 4 chars which are stored in an array
+	private char readUserInput() throws IOException {
+		
+		char input;
+		inputStreamReader.read(secretCodeInput, 0, 4);
+		return 0;
+		
+	}
+	
+	//Returns random number
 	private int getRandomNumber(int min, int max) {
 		
 	    int randomNumber = random.nextInt((max - min) + 1) + min;
@@ -40,13 +75,47 @@ public class Main {
 		
 	}
 	
-	private void printCode() {
+	private void printCodeGenerated() {
+		
+		for (int i = 0; i < secretCodeGenerated.length ; i++) {
+			
+			System.out.print(secretCodeGenerated[i]);
+			
+		}	
+		
+		System.out.println();
+		
+	}
+	
+	private void printCodeInput() {
 		
 		for (int i = 0; i < 4; i++) {
 			
-			System.out.println(secretCode[i]);
+			System.out.print(secretCodeInput[i]);
 			
 		}	
+		
+		System.out.println();
+		
+	}
+	
+	
+	//Validates input and returns if the validation was succesful (true) or not (fals)
+	//Has to be fixed -> runs loop 4 times no matter if it was succesful or not
+	private boolean validateInput() {
+		
+		for (int j = 0; j < secretCodeInput.length; j++) {
+			
+				if (secretCodeInput[j] != COLORS[0] && secretCodeInput[j] != COLORS[1] && secretCodeInput[j] != COLORS[2] && secretCodeInput[j] != COLORS[3] && secretCodeInput[j] != COLORS[4] && secretCodeInput[j] != COLORS[5]) {
+					
+					System.out.println("In dem eingegeben Code sind Fehler vorhanden, bitte wiederholen: ");
+					return false;
+					
+				}
+			
+		}
+			
+		return true;
 		
 	}
 
